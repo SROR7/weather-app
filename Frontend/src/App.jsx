c5d041c62411b10229c507432358578aaa2da0e1
Mohamed_Sror <m7mdsror0@gmail.com>
2026-08-02 21:06:42 +0300

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       setLoading(true);
       setError("");
 
-      const response = await axios.get("http://localhost:3000/weather", {
+      const response = await axios.get("/api/weather", {
         params: {
           city: city,
         },
